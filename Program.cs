9e195153a800cada8db8c4f0e4b71f72d1095a07
AbhilashReddy11 <yennapu.abhilash@kellton.com>
2023-04-26 13:37:55 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿ using System;  
+using System.Linq;  
+using System.Collections.Generic;  
+  
+class AA
+{  
+  
+  
+    static public void Main()  
+    {  
+   
+        List<string> list = new List<string>()  
+        {  
+            " Hii JavaTpoint",  
+            " Welcome to JavaTpoint ",  
+            " The topic is LINQ ",  
+            " Any Queries "  
+  
+        };  
+          var k = list.Where(a=> a.Contains(" JavaTpoint "));  
+          foreach(var i in k)  
+        {  
+            Console.WriteLine(i);  
+        }  
+        
+        var r = from l in list  
+                where l.Contains(" JavaTpoint")  
+                select l;  
+  
+
+        foreach(var i in r)  
+        {  
+            Console.WriteLine(i);  
+        }  
+    }}
+
+
